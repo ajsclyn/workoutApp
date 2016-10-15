93a21a2da90be8cff6fae299f23531ccade12b52
AJ Saclayan <ajsaclayan@gmail.com>
2016-10-14 22:03:26 -0400

    Init commit

---
+++ javascript/index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by ajsaclayan on 10/14/16.
+ */
+var workouts = [
+    {exercise: "Pull",
+     visible: true},
+    {exercise: "Push",
+     visible: true},
+    {exercise: "Legs",
+     visible: true},
+    {exercise: "Other",
+     visible: true}];
